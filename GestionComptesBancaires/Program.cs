7fffb7daab6d2a3e9bd4b64e8c21d287cec1f3b8
MOUIGUINA MOHAMED <mohamedmouiguina7@gmail.com>
2020-12-09 19:55:25 +0100

    Add project files.

---
+++ GestionComptesBancaires/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GestionComptesBancaires
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
